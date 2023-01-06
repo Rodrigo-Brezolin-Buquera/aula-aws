f6b3b1d27539bedeafffe2f0d8744fa1add761aa
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-01-06 18:31:11 -0300

    aula aws

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { app } from "./app";
+import { createProduct } from "./endpoints/createProduct";
+import { createPurchase } from "./endpoints/createPurchase";
+import { createTicket } from "./endpoints/createTicket";
+import { createUser } from "./endpoints/createUser";
+import { getAllProducts } from "./endpoints/getAllProducts";
+import { getAllPurchases } from "./endpoints/getAllPurchase";
+import { getAllTickets } from "./endpoints/getAllTickets";
+import { getAllUsers } from "./endpoints/getAllUsers";
+import { getPurchasesByUserId } from "./endpoints/getPurchasesByUserId";
+import { Request, Response } from "express"
+
+
+app.get("/ping", async (req: Request, res: Response) => {
+    res.status(200).send("pong")
+})
+
+app.get("/users", getAllUsers)
+app.get("/products", getAllProducts)
+app.get("/tickets", getAllTickets)
+app.get("/purchase", getAllPurchases)
+app.get("/purchase/:id", getPurchasesByUserId)
+
+app.post("/users", createUser)
+app.post("/products", createProduct)
+app.post("/tickets", createTicket)
+app.post("/purchase", createPurchase)
